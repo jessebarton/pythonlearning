4df1035bac6b8365a76231c59763db6aa23438c8
Brenton Neal <brenton.neal@life.church>
2019-05-03 13:46:47 -0500

    move commaCode to mydir and fix forLoop

--- brenton/commaCode.py
+++ brenton/commaCode.py
@@ -2,10 +2,9 @@ myList = []
 
 while True:
     print('Enter an item to be added to the list: ' + str(len(myList) +1) + ' (Or enter nothing to stop.):')
-    item = input()
+    item = input() #add input items to the list
     if item == '':
         break
     myList = myList +[item] #concatenate list
 print('Items in the list are:')
-for Items in myList:
-        print(*myList, sep = ", ") 
+print(*myList, sep = ", ") 
