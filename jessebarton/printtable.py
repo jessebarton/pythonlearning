eea597588b0ce3543997e0b452c2abfe5e06108f
jessebarton <jessebarton95@gmail.com>
2019-05-03 15:30:30 -0500

    printtable added

--- jessebarton/printtable.py
+++ jessebarton/printtable.py
@@ -3,11 +3,12 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-def printTable(tableData):
-    # colWidths = [0] * len(tableData)
-    for data in tableData:
-        for d in data:
-            print(d.rjust(10))
+def printTable(table):
+    for k in range(len(table[0])):
+        for v in range(len(table)):
+            print(table[v][k].rjust(10), end = ' ')
+        print()
+
 
 if __name__ == "__main__":
     printTable(tableData)
